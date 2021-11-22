34f57a963e9f26ddfcec3413eb658e5cc3e911e7
Antoine Viallon <antoine@lesviallon.fr>
2021-11-22 17:12:37 +0100

    Better time formatting

--- scaling/client/client.sh
+++ scaling/client/client.sh
@@ -3,15 +3,17 @@
 DIR=$(dirname "${BASH_SOURCE[0]}")
 
 if [ $# -ne 2 ]; then
-    echo "usage: host:port #calls";
+    echo "usage: $(basename '$0') host:port #calls";
     exit 1;
 fi
 
+mkdir -p client || true
+
 clientID="client#$(( ( RANDOM % 1000 )  + 1 ))"
 rm -f ${clientID}.dat
-host=$1
-ncalls=$2
-for i in `seq 1 ${ncalls}`
-do
-    { time ${DIR}/request.sh ${host}; } 2>&1 | grep real >> ${clientID}.dat
+host="$1"
+ncalls="$2"
+for i in `seq 1 ${ncalls}`; do
+    /bin/env time -a -o client/${clientID}.dat -f '%e' -- "${DIR}"/request.sh ${host} 2>&1 >/dev/null
 done
+rm -f *.pdf
